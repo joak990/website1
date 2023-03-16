481c7d4dba7bcf69900b175394d7724e7e8b20ef
joak990 <joakhaidar@gmail.com>
2023-03-16 01:47:09 -0300

    first project

---
+++ myapp/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {colors:{
+      primary: "#706FE5",
+      secondary:"#EAEAFC",
+    }},
+  },
+  plugins: [],
+}
